2bc6a5287b7152663f3fb9a411feb1f6026221ec
hiquezsy <pedrohenriqueborba1@gmail.com>
2026-08-12 23:55:35 -0300

    Começando o projeto

---
+++ src/main/java/Sistema/de/Gestao/de/Consultas/Medicas/Application.java
@@ -0,0 +1,13 @@
+package Sistema.de.Gestao.de.Consultas.Medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
